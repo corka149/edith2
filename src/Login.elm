9490e36528b84a37f5d039f5fb6efaed060af8dc
corka149 <corka149@mailbox.org>
2019-11-18 14:49:32 +0100

    Switch from async to form submission in Login

--- src/Login.elm
+++ src/Login.elm
@@ -6,7 +6,7 @@ import Bootstrap.Grid as BsGrid
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onClick)
+import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Encode as JE
 
@@ -43,24 +43,24 @@ type Msg
     = GotText (Result Http.Error String)
     | Password String
     | Email String
-    | TryAuthentication
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Email email ->
-            ( { model | email = email}, Cmd.none)                
+            ( { model | email = email }, Cmd.none )
+
         Password password ->
-            ( { model | password = password}, Cmd.none)
+            ( { model | password = password }, Cmd.none )
+
         GotText result ->
             case result of
                 Ok responseText ->
-                    ( { model | authenticated = True}, Cmd.none)            
+                    ( { model | authenticated = True }, Cmd.none )
+
                 Err _ ->
-                    ( { model | authenticated = False}, Cmd.none)
-        TryAuthentication  ->
-            (model, postCredentials model.email model.password)
+                    ( { model | authenticated = False }, Cmd.none )
 
 
 
@@ -82,22 +82,28 @@ view model =
         [ BsCDN.stylesheet -- creates an inline style node with the Bootstrap CSS
         , BsGrid.row []
             [ BsGrid.col []
-                [ Html.form [ style "background-color" "#ff9900", style "border-radius" "10px", class "py-2" ]
+                [ Html.form
+                    [ style "background-color" "#ff9900"
+                    , style "border-radius" "10px"
+                    , class "py-2"
+                    , action "/jarvis/auth/signin"
+                    , method "post"
+                    ]
                     [ div [ class "form-group", class "py-2", class "px-4" ]
                         [ label [ for "emailinput" ] [ text "E-Mail" ]
-                        , input [ id "emailinput", placeholder "E-Mail", class "form-control", type_ "email", required True, onInput Email ] []
+                        , input [ id "emailinput", placeholder "E-Mail", class "form-control", type_ "email", required True, onInput Email, name "email" ] []
                         ]
                     , div [ class "form-group", class "py-2", class "px-4" ]
                         [ label [ for "passwordinput" ] [ text "Password" ]
-                        , input [ id "passwordinput", placeholder "Password", class "form-control", type_ "password", required True, onInput Password ] []
+                        , input [ id "passwordinput", placeholder "Password", class "form-control", type_ "password", required True, onInput Password, name "password" ] []
                         ]
                     , div [ class "py-2", class "px-4", class "d-flex", class "justify-content-end" ]
-                        [ BsButton.button [ BsButton.primary, BsButton.attrs [ type_ "button", onClick TryAuthentication ] ] [ text "Submit" ]
+                        [ BsButton.button [ BsButton.primary, BsButton.attrs [] ] [ text "Submit" ]
                         ]
                     ]
                 ]
             ]
-            , text (isAuthenticated model.authenticated)
+        , text (isAuthenticated model.authenticated)
         ]
 
 
@@ -105,24 +111,10 @@ view model =
 -- UTILS
 
 
-isAuthenticated authenticated = 
+isAuthenticated authenticated =
     case authenticated of
         True ->
-            "Is authenticated"    
+            "Is authenticated"
+
         False ->
             "Unauthenticated"
-
-
-postCredentials : String -> String -> Cmd Msg
-postCredentials email password =
-    Http.post
-    { url = "http://localhost:4000/auth/signin"
-    , body = Http.jsonBody <| credentialsEncoder email password
-    , expect = Http.expectString GotText
-    }
-
-
-credentialsEncoder email password =
-    JE.object [ ("email", JE.string email)
-    ,    ("password", JE.string password)
-    ]
