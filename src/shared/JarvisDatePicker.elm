7498b85eab5783915a63b88dd5f1a4efde6dbdd4
corka149 <corka149@mailbox.org>
2019-11-23 17:22:51 +0100

    Added first version of JarvisDatePicker

---
+++ src/shared/JarvisDatePicker.elm
@@ -0,0 +1,119 @@
+module JarvisDatePicker exposing (main)
+--
+-- Inspired from bootstrap example https://package.elm-lang.org/packages/CurrySoftware/elm-datepicker/latest/
+--
+
+
+import Bootstrap.CDN as BsCDN
+import Browser
+import Date exposing (Date, day, month, weekday, year)
+import DatePicker exposing (DateEvent(..), defaultSettings)
+import Html exposing (Html, div, form, h1, input, label, text)
+import Html.Attributes exposing (class, type_, value)
+import Time exposing (Weekday(..))
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = \_ -> init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { date : Maybe Date
+    , datePicker : DatePicker.DatePicker
+    }
+
+
+settings : DatePicker.Settings
+settings =
+    let
+        isDisabled date =
+            [ Sat, Sun ]
+                |> List.member (weekday date)
+    in
+    { defaultSettings
+        | isDisabled = isDisabled
+        , inputClassList = [ ( "form-control", True ) ]
+        , inputName = Just "date"
+        , inputId = Just "date-field"
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    let
+        ( datePicker, datePickerFx ) =
+            DatePicker.init
+    in
+    ( { date = Nothing
+      , datePicker = datePicker
+      }
+    , Cmd.map ToDatePicker datePickerFx
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ToDatePicker DatePicker.Msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg ({ datePicker } as model) =
+    case msg of
+        ToDatePicker subMsg ->
+            let
+                ( newDatePicker, event ) =
+                    DatePicker.update settings subMsg datePicker
+            in
+            ( { model
+                | date =
+                    case event of
+                        Picked date ->
+                            Just date
+
+                        _ ->
+                            model.date
+                , datePicker = newDatePicker
+              }
+            , Cmd.none
+            )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view ({ date, datePicker } as model) =
+    div [ class "col-md-3" ]
+        [ BsCDN.stylesheet -- creates an inline style node with the Bootstrap CSS
+        , form []
+            [ div [ class "form-group" ]
+                [ label [] [ text "Pick a date" ]
+                , DatePicker.view date settings datePicker
+                    |> Html.map ToDatePicker
+                ]
+            , input
+                [ type_ "submit"
+                , class "btn btn-primary"
+                , value "Submit"
+                ]
+                []
+            ]
+        ]
