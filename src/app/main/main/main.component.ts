6f19b7ba7b2eac81bda26b62c304ccfb97ad5062
corka149 <corka149@mailbox.org>
2019-12-15 09:06:01 +0100

    Improved logout

--- src/app/main/main/main.component.ts
+++ src/app/main/main/main.component.ts
@@ -1,32 +1,39 @@
 import {MediaMatcher} from '@angular/cdk/layout';
-import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+import {ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import { AuthenticationService } from 'src/app/account/services/authentication.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnDestroy, OnDestroy {
+export class MainComponent implements OnInit, OnDestroy {
   mobileQuery: MediaQueryList;
   authService: AuthenticationService;
+  router: Router;
 
   private mobileQueryListener: () => void;
   private subscribtions =  new Subscription();
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, authService: AuthenticationService) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, authService: AuthenticationService,
+              router: Router) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this.mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this.mobileQueryListener);
     this.authService = authService;
+    this.router = router;
+  }
+
+  ngOnInit(): void {
+    this.subscribtions.add(
+      this.authService.getUserDetails().subscribe()
+    );
   }
 
   ngOnDestroy(): void {
     this.mobileQuery.removeListener(this.mobileQueryListener);
-  }
-
-  ngOnDestory(): void {
     this.subscribtions.unsubscribe();
   }
 
@@ -34,7 +41,7 @@ export class MainComponent implements OnDestroy, OnDestroy {
     this.subscribtions.add(this.authService.logout().subscribe(
       successful => {
         if (successful) {
-          window.location.reload();
+          this.router.navigateByUrl('/');
         }
       }
     ));
